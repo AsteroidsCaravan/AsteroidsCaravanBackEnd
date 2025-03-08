a39c2c8421bf7db4e4074a754703c6315903b1ac
JenniferDaleffi <jenniferdaleffii@gmail.com>
2025-03-08 19:48:59 -0300

    class model and searchHistory

--- src/main/java/asteroids_caravan/model/SearchHistory.java
+++ src/main/java/asteroids_caravan/model/SearchHistory.java
@@ -7,4 +7,17 @@ public class SearchHistory {
     private int idSearchHistory;
     private DateTime surveyDay;
     
+    public int getIdSearchHistory() {
+        return idSearchHistory;
+    }
+    public void setIdSearchHistory(int idSearchHistory) {
+        this.idSearchHistory = idSearchHistory;
+    }
+    public DateTime getSurveyDay() {
+        return surveyDay;
+    }
+    public void setSurveyDay(DateTime surveyDay) {
+        this.surveyDay = surveyDay;
+    }
+
 }
