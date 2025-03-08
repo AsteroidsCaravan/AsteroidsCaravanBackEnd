f895fa48de9592f238615b073aa64e307ce76e70
Leonardo-dev-br <99846550+Leonardo-dev-br@users.noreply.github.com>
2025-03-08 14:25:31 -0300

    WIP: Implementando Controller! Falta o Get

--- src/main/java/asteroids_caravan/controller/SearchHistoryController.java
+++ src/main/java/asteroids_caravan/controller/SearchHistoryController.java
@@ -1,4 +1,26 @@
 package asteroids_caravan.controller;
 
+import asteroids_caravan.model.SearchHistory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
 public class SearchHistoryController {
+    private  List<SearchHistory> searchHistory = new ArrayList<>();
+
+    @GetMapping("/history")
+    public List<SearchHistory> index(){
+        return searchHistory;
+    }
+
+    @PostMapping("/history")
+    public ResponseEntity create(@RequestBody SearchHistory history){
+        searchHistory.add(history);
+        System.out.println("Histórico registrado " + history);
+        return ResponseEntity.status(201).body(history);
+    }
 }
