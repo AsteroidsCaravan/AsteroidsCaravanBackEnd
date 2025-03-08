4681142907a7d3803c2bfbb0a8b1cd1037e7eae2
JenniferDaleffi <jenniferdaleffii@gmail.com>
2025-03-08 19:43:06 -0300

    master > main

--- src/main/java/asteroids_caravan/model/SearchHistory.java
+++ src/main/java/asteroids_caravan/model/SearchHistory.java
@@ -3,4 +3,8 @@ package asteroids_caravan.model;
 public class SearchHistory {
     //IMPLEMENTAR ATRIBUTOS
     //IMPLEMENTAR GETTERS E SETTERS
+
+    private int idSearchHistory;
+    private DateTime surveyDay;
+    
 }
